9becfc241d2735d8794128e46174c8416a0f3722
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-09 12:37:07 +0100

    test

--- employee.py
+++ employee.py
@@ -12,13 +12,19 @@ class Employee:
   
     def str(self):
         str = ""
-        billie.str == "Billie works on a monthly salary of 4000.\s+Their total pay is 4000.$" 
-        charlie.str == "Charlie works on a contract of 100 hours at 25/hour. Their total pay is 2500."
-        renee.str == "Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract. Their total pay is 3800."
-        jan.str =="Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract. Their total pay is 4410." 
-        robbie.str == "Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is 3500." 
-        ariel.str == "Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is 4200."
-        
+        if self.name == ('Billie'):
+            str = "Billie works on a monthly salary of 4000.\s+Their total pay is 4000.$" 
+        elif self.name == ('Charlie'):
+            str = "Charlie works on a contract of 100 hours at 25/hour. Their total pay is 2500."
+        elif self.name == ('Renee'):
+            str == "Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract. Their total pay is 3800."
+        elif self.name == ('Jan'):
+            str ="Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract. Their total pay is 4410." 
+        elif self.name == ('Robbie'):
+            str = "Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is 3500." 
+        else:
+            str = "Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is 4200."
+        return str
 
 
     def get_pay(self):
@@ -27,8 +33,16 @@ class Employee:
             pay = 4000
         elif self.name == ('Charlie'):
             pay = 2500
+        elif self.name == ('Renee'):
+            pay = 3800
+        elif self.name == ('Jan'):
+            pay = 4410
+        elif self.name == ('Robbie'):
+            pay = 3500
+        elif self.name == ('Ariel'):
+            pay = 4200
         return pay
-        
+
     def __str__(self):
         return self.name
 
