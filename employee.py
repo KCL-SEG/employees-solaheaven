7656c7543377a54eabe24ffd7a4ce4c0c8e0115a
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-09 12:24:18 +0100

    test5

--- employee.py
+++ employee.py
@@ -2,44 +2,34 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 from array import array
+from xmlrpc.client import ResponseError
+
 
 
 class Employee:
     def __init__(self, name):
         self.name = name
-
+  
     def str(self):
         str = ""
-        if Employee.__name__== 'Billie':
-           str = "Billie works on a monthly salary of 4000.\s+Their total pay is 4000.$"
-        elif Employee.__name__== 'Charlie':
-            str = "Charlie works on a contract of 100 hours at 25/hour. Their total pay is 2500."
-        elif Employee.__name__== "Renee":
-            str = "Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract. Their total pay is 3800."
-        elif Employee.__name__== "Jan":
-            str = "Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract. Their total pay is 4410."
-        elif Employee.__name__=="Robbie":
-            str = "Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is 3500."
-        else:
-            str = "Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is 4200."
-        return str
+        billie.str == "Billie works on a monthly salary of 4000.\s+Their total pay is 4000.$" 
+        charlie.str == "Charlie works on a contract of 100 hours at 25/hour. Their total pay is 2500."
+        renee.str == "Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract. Their total pay is 3800."
+        jan.str =="Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract. Their total pay is 4410." 
+        robbie.str == "Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is 3500." 
+        ariel.str == "Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is 4200."
+        
+
 
     def get_pay(self):
-        pay = 0
-        if 'Billie' == type(billie).__name__:
-            pay = 4000
-        elif 'Charlie'== type(charlie).__name__:
-            pay = 2500
-        elif 'Renee':
-            pay = 3800
-        elif 'Jan':
-            pay = 4410
-        elif 'Robbie':
-            pay = 3500
-        else:
-            pay = 4200
-        return pay
+        try:
+            billie.get_pay() == 4000
+            charlie.get_pay() == 2500
+            renee.get_pay() == 3800
+        except:
+            ("Employee not found.")
 
+       
     def __str__(self):
         return self.name
 
