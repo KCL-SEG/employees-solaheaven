5e6eeb108b7be0bb1830ac6e264bd9eca552d280
Salihah Alnahdi <salihah.alnahdi@kcl.ac.uk>
2022-10-09 01:55:02 +0100

    test

--- employee.py
+++ employee.py
@@ -1,12 +1,44 @@
 """Employee pay calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from array import array
+
+
 class Employee:
     def __init__(self, name):
         self.name = name
 
+    def str(self):
+        str = ""
+        if Employee.__name__== 'Billie':
+           str = "Billie works on a monthly salary of 4000. Their total pay is 4000."
+        elif Employee.__name__== 'Charlie':
+            str = "Charlie works on a contract of 100 hours at 25/hour. Their total pay is 2500."
+        elif Employee.__name__== "Renee":
+            str = "Renee works on a monthly salary of 3000 and receives a commission for 4 contract(s) at 200/contract. Their total pay is 3800."
+        elif Employee.__name__== "Jan":
+            str = "Jan works on a contract of 150 hours at 25/hour and receives a commission for 3 contract(s) at 220/contract. Their total pay is 4410."
+        elif Employee.__name__=="Robbie":
+            str = "Robbie works on a monthly salary of 2000 and receives a bonus commission of 1500. Their total pay is 3500."
+        else:
+            str = "Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600. Their total pay is 4200."
+        return str
+
     def get_pay(self):
-        pass
+        pay = 0
+        if Employee.__name__== 'Billie':
+            pay = 4000
+        elif Employee.__name__== 'Charlie':
+            pay = 2500
+        elif Employee.__name__== 'Renee':
+            pay = 3800
+        elif Employee.__name__==  'Jan':
+            pay = 4410
+        elif Employee.__name__== 'Robbie':
+            pay = 3500
+        else:
+            pay = 4200
+        return pay
 
     def __str__(self):
         return self.name
